d4606029a1a6a94116145dd9a6a7faa9ff674868
ajc32199 <adam.chiarella@gmail.com>
2025-01-10 21:22:09 -0600

    text entry UI created

---
+++ src/components/login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './login.css';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password', password);
+    };
+    return(
+        <div className="login-page">
+            <form onSubmit ={handleSubmit}>
+                <h2>TKE Login</h2>
+                <div className = "form-group">
+                    <label htmlFor="email">email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    ); 
+};
+
+export default LoginPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LoginPage from './components/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className="App">
+      <LoginPage />
+    </div>
   </StrictMode>,
 )
